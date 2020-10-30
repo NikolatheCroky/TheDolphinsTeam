30d4ec61999b050b70f693f287cc23d5874cc6c4
NikolatheCroky <66215820+NikolatheCroky@users.noreply.github.com>
2020-10-30 20:55:42 +0200

    Create main.js

---
+++ public/JS/main.js
@@ -0,0 +1,11 @@
+function onclick_event0() {
+    window.location.replace('http://localhost:3000/portfolio/index0');
+}
+
+function onclick_event1() {
+    window.location.replace('http://localhost:3000/portfolio/index1');
+}
+
+function onclick_event2() {
+    window.location.replace('http://localhost:3000/portfolio/index2');
+}
